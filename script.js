c687f44323fb9bdd6b62743b2494660246ec1c76
Emmanuel-Lima-dev <lima_kpo@hotmail.com>
2024-02-15 21:22:25 -0300

    finish

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const navbarToggler = document.querySelector('.navbar-toggler');
 const navbarCollapse = document.querySelector('.navbar-collapse');
 const dropdownLinks = document.querySelectorAll('.navbar-nav .nav-link');
 const telefono = document.querySelector('.telefono');
+const botonWhatsAppPrincipal = document.querySelector()
 
 // Función para actualizar las clases del navbar
 function updateNavbarClasses() {
