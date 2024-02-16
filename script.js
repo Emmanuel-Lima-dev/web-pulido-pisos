3ac9d9433883d446fb055659484d5ffe0a0d1b87
Emmanuel-Lima-dev <lima_kpo@hotmail.com>
2024-02-15 21:12:57 -0300

    terminado

--- script.js
+++ script.js
@@ -1,94 +1,82 @@
-let navBar = document.querySelector(".navbar"),
-contTextLogo = document.querySelector(".cont-text-logo"),
-formulario = document.querySelector("#formulario");
+// Seleccionar elementos del DOM una sola vez al cargar el script
+const navBar = document.querySelector(".navbar");
+const contTextLogo = document.querySelector(".cont-text-logo");
+const formulario = document.querySelector("#formulario");
+const navbarToggler = document.querySelector('.navbar-toggler');
+const navbarCollapse = document.querySelector('.navbar-collapse');
+const dropdownLinks = document.querySelectorAll('.navbar-nav .nav-link');
+const telefono = document.querySelector('.telefono');
 
-window.addEventListener('load', function() {
-    updateNavbarClasses(); // Llama a la función para establecer las clases del navbar al cargar la página
-
-    window.addEventListener('resize', function() {
-        updateNavbarClasses(); // Llama a la función cada vez que cambie el tamaño de la ventana
-    });
-});
-
+// Función para actualizar las clases del navbar
 function updateNavbarClasses() {
-    let navbar = document.querySelector('.navbar');
-    let windowWidth = window.innerWidth;
-
+    const windowWidth = window.innerWidth;
     if (windowWidth <= 768) {
-        navbar.classList.remove('navbar-dark');
-        navbar.classList.add('navbar-light');
+        navBar.classList.remove('navbar-dark');
+        navBar.classList.add('navbar-light');
         contTextLogo.classList.add('text-dark')
         contTextLogo.classList.remove("text-white")
     } else {
-        navbar.classList.remove('navbar-light');
-        navbar.classList.add('navbar-dark');
+        navBar.classList.remove('navbar-light');
+        navBar.classList.add('navbar-dark');
         contTextLogo.classList.remove('text-dark')
         contTextLogo.classList.add("text-white")
     }
 }
 
+// Evento al cargar la página y al cambiar el tamaño de la ventana para actualizar las clases del navbar
+window.addEventListener('load', updateNavbarClasses);
+window.addEventListener('resize', updateNavbarClasses);
 
-
-window.addEventListener("scroll", ()=>{
+// Evento de scroll para ajustar las clases del navbar
+window.addEventListener("scroll", () => {
     if (window.innerWidth > 768) {
         if (window.scrollY === 0) {
-            navBar.classList.add("navBarTop")
-            navBar.classList.add("py-5")
-            navBar.classList.add("navbar-dark")
-            navBar.classList.remove("navBarBottom")
-            contTextLogo.classList.add("text-white")
-            contTextLogo.classList.remove("text-dark")
-    
+            navBar.classList.add("navBarTop", "py-5", "navbar-dark");
+            navBar.classList.remove("navBarBottom");
+            contTextLogo.classList.add("text-white");
+            contTextLogo.classList.remove("text-dark");
         } else if (window.scrollY > 0) {
-            navBar.classList.remove("navBarTop")
-            navBar.classList.add("navBarBottom")
-            navBar.classList.remove("py-5")
-            navBar.classList.remove("navbar-dark")
-            contTextLogo.classList.add("text-dark")
-            contTextLogo.classList.remove("text-white")
+            navBar.classList.remove("navBarTop");
+            navBar.classList.add("navBarBottom");
+            navBar.classList.remove("py-5", "navbar-dark");
+            contTextLogo.classList.add("text-dark");
+            contTextLogo.classList.remove("text-white");
         }
     }
-
-    
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    // Obtener el botón del navbar y el menú desplegable
-    var navbarToggler = document.querySelector('.navbar-toggler');
-    var navbarCollapse = document.querySelector('.navbar-collapse');
-  
-    // Agregar un evento de clic al documento
-    document.addEventListener('click', function (event) {
-      // Verificar si el clic no ocurrió dentro del menú desplegable y el botón del navbar
-      if (!navbarCollapse.contains(event.target) && !navbarToggler.contains(event.target)) {
+// Evento de clic en el documento para cerrar el menú desplegable si está abierto
+document.addEventListener('click', (event) => {
+    // Verificar si el clic ocurrió dentro del menú desplegable o en el botón del navbar
+    const isClickInsideNavbar = navbarCollapse.contains(event.target) || navbarToggler.contains(event.target);
+    
+    if (!isClickInsideNavbar) {
         // Cerrar el menú desplegable si está abierto
-        if (!navbarCollapse.classList.contains('collapsed')) {
-          navbarToggler.click();
+        if (navbarCollapse.classList.contains('show')) {
+            navbarToggler.click();
         }
-      }
-    });
-  
-    // Obtener todos los enlaces del menú desplegable
-    var dropdownLinks = document.querySelectorAll('.navbar-nav .nav-link');
-  
-    // Agregar evento de clic para cada enlace del menú desplegable
-    dropdownLinks.forEach(function (link) {
-      link.addEventListener('click', function () {
-        // Cerrar el menú desplegable
+    }
+});
+
+// Evento de clic para cerrar el menú desplegable al hacer clic en los enlaces del menú
+dropdownLinks.forEach((link) => {
+    link.addEventListener('click', () => {
         navbarToggler.click();
-      });
     });
-  });
-  
+});
 
+telefono.addEventListener("keydown", (event) => {
+  if (event.key.length === 1 && !/\d|-|\./.test(event.key)) {
+    event.preventDefault();
+  }
+});
 
-
-formulario.addEventListener('submit', function(e) {
+// Evento de envío del formulario
+formulario.addEventListener('submit', (e) => {
     e.preventDefault(); 
-    let nombre = document.getElementById('nombre').value;
-    let telefono = document.getElementById('telefono').value;
-    let mensaje = document.getElementById('mensaje').value;
-    let textoWhatsapp = 'Hola, soy ' + nombre + '. Mi teléfono es ' + telefono + '. Mensaje: ' + mensaje;
-    let linkWhatsapp = 'https://wa.me/5491128980237?text=' + encodeURIComponent(textoWhatsapp);
+    const nombre = document.querySelector('.nombre').value;
+    const mensaje = document.querySelector('.mensaje').value;
+    const textoWhatsapp = `Hola, soy ${nombre}.\nMi teléfono es ${telefono.value}.\nMensaje: ${mensaje}`;
+    const linkWhatsapp = `https://wa.me/5491128980237?text=${encodeURIComponent(textoWhatsapp)}`;
     window.open(linkWhatsapp, '_blank');
 });
